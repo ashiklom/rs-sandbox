1730cadb21e818bf1d73e562026c6b8130f145d8
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2026-01-02 17:24:50 -0500

    Add CMR and SMAP utils and examples

---
+++ code_snippets/smap-analyze.R
@@ -0,0 +1,22 @@
+box::use(./utils/smap)
+
+ca_shapefile <- "/mnt/c/Users/alexe/Downloads/pecan/ca_state/CA_State.shp"
+
+outdir <- "data-raw"
+smapfiles <- list.files(outdir, full.names = TRUE)
+smap_list <- purrr::map(smapfiles, smap$smap_raster)
+
+california <- terra::vect(ca_shapefile) |>
+  terra::project(smap_list[[1]][["raster"]])
+
+smap_cali_rast <- smap_list |>
+  purrr::map("raster") |>
+  purrr::map(terra::crop, y = california, mask = TRUE)
+
+sm_times <- purrr::map_vec(smap_list, "time")
+sm_list <- purrr::map(smap_cali_rast, ~ .x[["sm_rootzone"]]) |>
+  purrr::map(stars::st_as_stars, proxy = TRUE)
+sm <- do.call(c, c(sm_list, along = "time"))
+stars::st_dimensions(sm)[["time"]][["values"]] <- sm_times
+
+plot(sm)
---
+++ code_snippets/smap-download.R
@@ -0,0 +1,29 @@
+box::use(./utils/cmr)
+
+start_date <- "2018-01-01"
+end_date <- "2019-01-01"
+
+site_info <- list(
+  site_id = "losthills",
+  lat = 35.5103,
+  lon = -119.6675,
+  start_date = "1999-01-01",
+  end_date = "2012-12-31"
+)
+
+search <- cmr$search_granules(
+  "C3480440870-NSIDC_CPRD",
+  point = c(site_info[["lon"]], site_info[["lat"]]),
+  temporal = c(start_date, end_date)
+)
+
+urls <- search |>
+  dplyr::filter(
+    stringr::str_starts(link_title, "Download"),
+    stringr::str_ends(link_href, ".h5")
+  ) |>
+  dplyr::pull(link_href) |>
+  head(10)
+
+outdir <- "data-raw"
+resps <- cmr$download_parallel(urls, outdir)
